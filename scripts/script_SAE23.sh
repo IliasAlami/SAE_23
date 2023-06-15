5f6c5758f99a306d907f95fe8b2a02f80c7d025c
suisse-a <suisse.anthony31@gmail.com>
2023-06-15 20:42:32 +0200

    correction bash

--- scripts/script_SAE23.sh
+++ scripts/script_SAE23.sh
@@ -12,24 +12,20 @@ for ((i=0; i<$nombre_batiment; i++)); do
     
     # Retrieve the id_batiment for the current iteration
     batiment=$(echo "SELECT id_batiment FROM sae23.batiment LIMIT $i,1;" | /opt/lampp/bin/mysql -h localhost -u root -ppassroot  2>/dev/null| tail -1)
-
-    ##echo  batiment $batiment
-    
+ 
     # Retrieve the total number of rooms in the sae23.capteur table in a building
-    nombre_room=$(echo "SELECT COUNT(salle) FROM sae23.capteur WHERE id_batiment=${batiment};" | /opt/lampp/bin/mysql -h localhost -u root -ppassroot 2>/dev/null| tail -1)
+    nombre_room=$(echo "SELECT COUNT(salle) FROM sae23.capteur WHERE id_batiment='${batiment}';" | /opt/lampp/bin/mysql -h localhost -u root -ppassroot 2>/dev/null| tail -1)
 
     # Loop through each room in the current building
     for ((x=0; x<$nombre_room; x++)); do
 
    	 # Retrieve the salle value for the current building in sae23.capteur table
-   	 room=$(echo "SELECT salle FROM sae23.capteur WHERE id_batiment=${batiment} LIMIT $x,1;" | /opt/lampp/bin/mysql -h localhost -u root -ppassroot  2>/dev/null| tail -1)
-
-   	 ##echo room $room
+   	 room=$(echo "SELECT salle FROM sae23.capteur WHERE id_batiment='${batiment}' LIMIT $x,1;" | /opt/lampp/bin/mysql -h localhost -u root -ppassroot  2>/dev/null| tail -1)
 
    	 # Subscribe to MQTT topic and get the latest data
    	 data=$(mosquitto_sub -h mqtt.iut-blagnac.fr -t "Student/by-room/$room/data" -C 1)
 
-   	 ##echo $data
+
 
    	 # Get the current system date and time
    	 date=$(date "+%F")
@@ -40,12 +36,9 @@ for ((i=0; i<$nombre_batiment; i++)); do
    	 co2=$(echo "$data" | jq '.[0].co2')
    	 capteur=$(echo "$data" | jq '.[1].devEUI' | sed -e 's/"//g')
 
-   	 ##echo "capteur "$capteur
 
    	 # Retrieve the type of the current sensor
    	 typeCapt=$(echo "SELECT type FROM sae23.capteur WHERE salle='${room}';" | /opt/lampp/bin/mysql -h localhost -u root -ppassroot  2>/dev/null | tail -1)
-   	 
-   	 ##echo "type : $typeCapt"
 
    	 # Check the sensor type and insert value in the table sae23.mesure
    	 case $typeCapt in
